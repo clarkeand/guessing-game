d634b19fe07eaab735a502b1bc6ea2247d5a2538
clarkeand <andclarke95@icloud.com>
2023-04-24 17:17:11 -0600

    Initial commit

---
+++ game.py
@@ -0,0 +1,18 @@
+"""A number-guessing game."""
+import random
+name = input("Howdy what's your name?")
+print(f"{name} I'm thinking of a number between 1 and 100.")
+print("Can you guess my number?")
+secretnum = random.randint(1,101)
+guesses = 0
+while True:
+    guess = input("Whats your guess?")
+    guess = int(guess)
+    guesses = guesses + 1
+    if guess > secretnum:
+        print("Sorry, too high")
+    if guess < secretnum:
+        print("Sorry, too low")
+    if guess == secretnum:
+        print(f"Well done {name}, you got it in {guesses} tries!")
+        break
