3e0e53056168c1ed8a57917e91a05134f731fa5e
clarkeand <superclarke909@gmail.com>
2023-04-24 18:03:38 -0600

    final with play again

--- game.py
+++ game.py
@@ -1,18 +1,26 @@
 """A number-guessing game."""
 import random
 name = input("Howdy what's your name?")
-print(f"{name} I'm thinking of a number between 1 and 100.")
-print("Can you guess my number?")
-secretnum = random.randint(1,101)
-guesses = 0
-while True:
-    guess = input("Whats your guess?")
-    guess = int(guess)
-    guesses = guesses + 1
-    if guess > secretnum:
-        print("Sorry, too high")
-    elif guess < secretnum:
-        print("Sorry, too low")
-    elif guess == secretnum:
-        print(f"Well done {name}, you got it in {guesses} tries!")
+while True: 
+    print(f"{name} I'm thinking of a number between 1 and 100.")
+    print("Can you guess my number?")
+    secretnum = random.randint(1,101)
+    guesses = 0
+    while True:
+        guess = input("Whats your guess?")
+        guess = int(guess)
+        guesses = guesses + 1
+        if guess > secretnum:
+            print("Sorry, too high")
+        elif guess < secretnum:
+            print("Sorry, too low")
+        elif guess == secretnum:
+            print(f"Well done {name}, you got it in {guesses} tries!")
+            break
+    playAgain = input("Do you want to play again? (Y/N)")
+    if playAgain == 'N' or playAgain == 'n': 
         break
+    elif playAgain == 'Y' or playAgain == 'y': 
+        print("New Game Loading...")
+    else:
+        print("Invalid Character, starting the game again.")
