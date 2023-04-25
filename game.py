fb1f1367fd91c26acc92339bb528c3c092ef44d3
clarkeand <superclarke909@gmail.com>
2023-04-25 13:16:27 -0600

    add faulty user input

--- game.py
+++ game.py
@@ -1,24 +1,28 @@
 """A number-guessing game."""
 import random
 name = input("Howdy what's your name?")
+save_guesses = 100 
 
 while True: 
     print(f"{name} I'm thinking of a number between 1 and 100.")
     print("Can you guess my number?")
     secretnum = random.randint(1,101)
+    # print(secretnum)
     guesses = 0
-    save_guesses = 100 
     while True:
         guess = input("Whats your guess?")
         guess = int(guess)
         guesses = guesses + 1
-        if guess > secretnum:
-            print("Sorry, too high")
-        elif guess < secretnum:
-            print("Sorry, too low")
-        elif guess == secretnum:
-            print(f"Well done {name}, you got it in {guesses} tries!")
-            break
+        if guess <= 100 and guess >=1:
+            if guess > secretnum:
+                print("Sorry, too high")
+            elif guess < secretnum:
+                print("Sorry, too low")
+            elif guess == secretnum:
+                print(f"Well done {name}, you got it in {guesses} tries!")
+                break
+        else: 
+            print("That is not within the range, don't cheat! Please enter a number between 1-100")
 
     if guesses < save_guesses: 
         save_guesses = guesses      
