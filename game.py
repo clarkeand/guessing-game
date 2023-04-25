ec7d1d72b87eee5583e22357719fb3959c7f4994
clarkeand <superclarke909@gmail.com>
2023-04-25 12:42:54 -0600

    inital game.py commit

--- game.py
+++ game.py
@@ -1,11 +1,14 @@
 """A number-guessing game."""
 import random
 name = input("Howdy what's your name?")
+
 while True: 
+
     print(f"{name} I'm thinking of a number between 1 and 100.")
     print("Can you guess my number?")
     secretnum = random.randint(1,101)
     guesses = 0
+
     while True:
         guess = input("Whats your guess?")
         guess = int(guess)
@@ -17,7 +20,9 @@ while True:
         elif guess == secretnum:
             print(f"Well done {name}, you got it in {guesses} tries!")
             break
+
     playAgain = input("Do you want to play again? (Y/N)")
+
     if playAgain == 'N' or playAgain == 'n': 
         break
     elif playAgain == 'Y' or playAgain == 'y': 
