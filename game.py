aeb23d39d2b116e4747c5f48eabe6c47bd05449f
clarkeand <superclarke909@gmail.com>
2023-04-25 13:30:14 -0600

    limit guesses

--- game.py
+++ game.py
@@ -6,10 +6,10 @@ save_guesses = 100
 while True: 
     print(f"{name} I'm thinking of a number between 1 and 100.")
     print("Can you guess my number?")
-    secretnum = random.randint(1,101)
+    secretnum = random.randint(1,100)
     # print(secretnum)
     guesses = 0
-    while True:
+    while guesses < 10:
         guess = input("Whats your guess?")
         if guess.isdigit():
             guess = int(guess)
@@ -21,12 +21,13 @@ while True:
                     print("Sorry, too low")
                 elif guess == secretnum:
                     print(f"Well done {name}, you got it in {guesses} tries!")
-                    break
             else: 
                 print("That is not within the range, don't cheat! Please enter a number between 1-100")
         else:
             print("Invalid input numbers only")
-            
+    if guesses == 10:
+        print("You ran out of guesses sorry")
+
     if guesses < save_guesses: 
         save_guesses = guesses      
     print(f"Your current high score is: {save_guesses}")
